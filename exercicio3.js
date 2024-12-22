aacae68a38e5ba264b373851ddbd9cf99856c447
Luiz Gustavo <142189729+Gust-Dev@users.noreply.github.com>
2024-12-22 12:08:54 -0300

    Add files via upload
    exercício logica - imc

---
+++ exercicio3.js
@@ -0,0 +1,17 @@
+const Peso = 60
+const Altura = 1.80
+let imc = Peso / (Altura*Altura)
+if(imc >=18.5 && imc <=25){
+    console.log("Peso normal")
+}else if(imc > 25 && imc <= 30 ){
+    console.log("Você esta a cima do peso")
+}else if(imc >30 && imc == 40){
+    console.log("você esta obeso")
+}else if(imc <18.5 ){
+    console.log("Você esta a baixo do peso")
+}else if(imc > 40){
+    console.log("Você tem obesidade gravissima e precisa de tratamento")
+}else{
+    console.log("informações invalidas")
+}
+console.log(imc.toFixed(2))
