0300d0140b8127b22fd43336d76b405b33900f79
Luiz Gustavo <142189729+Gust-Dev@users.noreply.github.com>
2024-12-22 13:40:50 -0300

    Add files via upload

---
+++ exercicio4.js
@@ -0,0 +1,18 @@
+
+
+let Produto = 1956
+
+const Debito =   (Produto - ((10/100) * Produto))
+const Avista =   (Produto - ((10/100) * Produto))  
+
+const Dinheiro = (Produto - ((15/100) * Produto))  
+const Pix      = (Produto - ((15/100) * Produto))  
+
+const Credito2x=Produto
+const Credito3x=((10/100) * Produto) + Produto
+
+const formas = [Debito, Avista,Dinheiro,Pix,Credito2x,Credito3x]
+const compradoNo = formas[5]
+
+console.log("Sua compra deu um total de:" + compradoNo)
+
