d3a088fbc088ce6c5d42028f4df35beaf7b7344e
Luiz Gustavo <142189729+Gust-Dev@users.noreply.github.com>
2024-12-22 18:39:58 -0300

    Add files via upload
    Exercício de função - verificador de idade

---
+++ exercicio6.js
@@ -0,0 +1,7 @@
+function VerificarIdade(idade){
+    if(idade >= 18){
+        console.log(`Voê tem ${idade} anos e é maior de idade`);
+    }  else 
+        console.log(`Você tem ${idade} anos e é menor de idade`);
+    }
+VerificarIdade(5)
