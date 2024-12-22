825736bbb7027d5ce3d1e639a1877a6ecb8220b7
Gust-Dev <dev.contato.luiz@gmail.com>
2024-12-22 18:51:28 -0300

    new

--- exercicio6.js
+++ exercicio6.js
@@ -8,9 +8,9 @@ function VerificarIdade(idade){
 =======
 function VerificarIdade(idade){
     if(idade >= 18){
-        console.log(`Voê tem ${idade} anos e é maior de idade`);
+        console.log(`Voê tem ${idade} anos e é maior de idade.`);
     }  else 
-        console.log(`Você tem ${idade} anos e é menor de idade`);
+        console.log(`Você tem ${idade} anos e é menor de idade.`);
     }
 >>>>>>> 0975906 (novo up)
 VerificarIdade(5)
