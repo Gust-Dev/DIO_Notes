c333b255566770b20b090e2082f4541b0e59d65f
Gust-Dev <dev.contato.luiz@gmail.com>
2024-12-22 21:46:15 -0300

    up

--- exercicio7.js
+++ exercicio7.js
@@ -2,13 +2,6 @@
 function Pagamento(Valor, TypeShop){
      TypeShop;
 
-10/100*Valor
-     
-//Debito = 0 10%
-//Dinheiro ou pix 15%
-//2x normal sem nada
-//3x +10% de taxa
-
 if (TypeShop == 0 ) {
     console.log(`Valor da compra: R$${-(10/100*Valor)+Valor} reais. Forma de pagamento:Debito`  )
          return
